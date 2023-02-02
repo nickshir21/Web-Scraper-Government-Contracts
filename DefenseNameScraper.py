63775bbf052e3d8fb7b0e692439d226083334bfe
nickshir21 <122573891+nickshir21@users.noreply.github.com>
2023-02-02 15:00:55 -0500

    General Changes
    Commented on code and fixed JSON output.

--- DefenseNameScraper.py
+++ DefenseNameScraper.py
@@ -2,47 +2,64 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+# must manually imput start page and end page using pageNumber and end
 pageNumber = 1
-end = 1000
-json_object = open(r"Company Name List.json", "a")
-
+end = 3
+open(r"TesterIndent.json", "r")
+dictionary = ''
+json_object = ''
+dictionaryArray = []
+#creates a loop that allows for the scraper to crawl multiple webpages.
 while pageNumber < end:
     s = requests.Session()
+    # saves url as a variable then adds the page number to the end. 
     url = "https://www.defense.gov/News/Contracts/?Page=" + str(pageNumber)
+    print(pageNumber)
     pageNumber = pageNumber + 1
     onlydate = ''
-    print(url)
     r = s.get(url)
     soup = BeautifulSoup(r.text, 'html5lib')
     links = []
+    # for loop that grabs the 10 relevant links on the webpage and saves them to an array called links
     for i in range(len(soup.find_all('listing-titles-only', attrs={'article-url' : True}))):
         links.append(soup.find_all('listing-titles-only', attrs={'article-url' : True})[i]['article-url'])
-        print(links)
-
+    
+    # loop that runs 10 times (only 10 links per page maximum)
     for i in range(10):
+        #iterates until the end of the "links" array
         webpage = s.get(links[i])
+        # creates a new object of just the html from one of the 10 links grabbed earlier.
         soup2 = BeautifulSoup(webpage.text, 'html5lib')
         data = ''
+        #grabs the date from the webpage then cleans excess
         header = soup2.find('h1').get_text()
         onlyDate = str(header.replace("            Contracts For ", ''))
         onlyDate = onlyDate.strip()
+        # grabs the "body" text from within the links.
         for data in soup2.find_all('p'):
+            # converts the data variable into a string then searches each paragraph for the word "awarded", and if it it is included then 
+            # it will grab all the characters until the first comma. 
             if 'awarded' in str(data):
                 dataInText = data.get_text()
                 (dataInText[:dataInText.index(",")])
+                # grabs everything before the comma
                 companyName = (dataInText[:dataInText.index(",")])
                 if 'CORRECTION' in companyName:
                     break;
                 try:
+                    # finds the index of the first dollar sign 
                     indexOfDollarSign = dataInText.index("$")
                 except:
                     break
                 award = ''
+                # loops and grabs each character.
                 for i in range(19): 
                     character = dataInText[indexOfDollarSign + i]
+                    # grabs the character then converts it into ASCII 
                     string = str(character)
                     charInt = ord(character)
-
+                    
+                    # checks each number one by one to verify they are numbers/commas/dollar sign
                     if ((charInt <= 57) & (charInt >= 48)):
                         award = award + character
                     elif (charInt == 36):
@@ -52,13 +69,15 @@ while pageNumber < end:
                     else:
                         break
                 
-                dictionary = {
-                    "Date": onlyDate,
-                    "Company Name": companyName,
-                    "Award Amount": award
-                }
-
+                # creates a json variable
+                dictionary = { "Company Name": companyName }
+                
+                # creates object with the json variable
                 json_object = json.dumps(dictionary, indent = 4)
-
-                with open("Company Name List.json", "a") as outfile:
-                    outfile.write(json_object)
+                dictionaryArray.append(json_object)
+                
+                
+                # writes into json file
+listConverter = json.dumps(dictionaryArray, indent = 4)
+with open("TesterIndent.json", "w") as outfile:
+    outfile.write(listConverter)
